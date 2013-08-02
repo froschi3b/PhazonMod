4c4f7af2fb5ca36883ef6fffc77e7fd0d9ba23e2
froschi3b <froschi3b@gmail.com>
2013-08-02 15:18:53 +0200

    Update

--- ph_common/at/flabs/mods/phazon/client/ProxyClient.java
+++ ph_common/at/flabs/mods/phazon/client/ProxyClient.java
@@ -9,6 +9,6 @@ public class ProxyClient extends ProxyCommon{
 
     public void registerRendering() {
         RenderingRegistry.registerEntityRenderingHandler(EntityInfPig.class, new RenderInfPig(0.7f));
-        RenderingRegistry.registerEntityRenderingHandler(EntityInfCow.class, new RenderInfCow(0.7f));
+        RenderingRegistry.registerEntityRenderingHandler(EntityInfCow.class, new RenderInfCow(0.7f)); 
         }
 }
