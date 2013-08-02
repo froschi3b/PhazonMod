72af97745c0536a4808f22cad36509a12c0b2c1c
froschi3b <froschi3b@gmail.com>
2013-08-02 02:11:24 +0200

    Adding Mod Class

--- ph_common/at/flabs/mods/phazon/PhazonMod.java
+++ ph_common/at/flabs/mods/phazon/PhazonMod.java
@@ -1,5 +1,44 @@
 package at.flabs.mods.phazon;
 
+import java.io.File;
+
+import net.minecraft.block.Block;
+import net.minecraftforge.common.Configuration;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.network.NetworkMod;
+
+@Mod(modid = Vars.modid, name = Vars.modname, version = Vars.version)
+@NetworkMod
 public class PhazonMod {
+    @Instance
+    public static PhazonMod instance;
+    public Block phazonBlock;
     
+    @EventHandler
+    public void perInit(FMLPreInitializationEvent evz) {
+        Configuration config = new Configuration(new File(evz.getModConfigurationDirectory(), "PhazonMod.cfg"));
+        try{
+            config.load();
+            
+        }catch(Exception e){
+            
+        }finally{
+            config.save();
+        }
+    }
+    
+    @EventHandler
+    public void init(FMLInitializationEvent evz) {
+        
+    }
+    
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent evz) {
+        
+    }
 }
---
+++ ph_common/at/flabs/mods/phazon/Vars.java
@@ -0,0 +1,7 @@
+package at.flabs.mods.phazon;
+
+public class Vars {
+    public static final String version = "1.0";
+    public static final String modid = "PhazonMod";
+    public static final String modname = "Phazon Mod";
+}
