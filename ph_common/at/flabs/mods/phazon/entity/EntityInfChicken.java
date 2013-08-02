7dd2e82cd4eff3a075b4985bd2932d90ccb1ef86
froschi3b <froschi3b@gmail.com>
2013-08-02 19:21:35 +0200

    Update

---
+++ ph_common/at/flabs/mods/phazon/entity/EntityInfChicken.java
@@ -0,0 +1,12 @@
+package at.flabs.mods.phazon.entity;
+
+import net.minecraft.world.World;
+
+public class EntityInfChicken extends EntityInf {
+
+    public EntityInfChicken(World par1World) {
+        super(par1World);
+        this.setSize(0.3F, 0.7F);
+    }
+    
+}
