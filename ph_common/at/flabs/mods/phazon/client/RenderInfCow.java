f41571128f117c9162d29ae130acf9173e9c7409
froschi3b <froschi3b@gmail.com>
2013-08-02 16:33:17 +0200

    Update

--- ph_common/at/flabs/mods/phazon/client/RenderInfCow.java
+++ ph_common/at/flabs/mods/phazon/client/RenderInfCow.java
@@ -1,11 +1,21 @@
 package at.flabs.mods.phazon.client;
 
+import at.flabs.mods.phazon.Vars;
 import net.minecraft.client.model.ModelCow;
+import net.minecraft.client.renderer.entity.RenderLiving;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.ResourceLocation;
 
-public class RenderInfCow extends RenderInf{
-
+public class RenderInfCow extends RenderLiving {
+    
     public RenderInfCow(float f) {
-        super(new ModelCow(), f,"cow");
+        super(new ModelCow(), f);
     }
     
+    private static final ResourceLocation tex = new ResourceLocation(Vars.texdir, "textures/entity/infcow.png");
+    
+    @Override
+    protected ResourceLocation func_110775_a(Entity entity) {
+        return tex;
+    }
 }
--- ph_common/at/flabs/mods/phazon/entity/EntityInfPig.java
+++ ph_common/at/flabs/mods/phazon/entity/EntityInfPig.java
@@ -1,22 +1,55 @@
 package at.flabs.mods.phazon.entity;
 
 import at.flabs.mods.phazon.PhazonMod;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.EntityAIAttackOnCollide;
+import net.minecraft.entity.ai.EntityAIHurtByTarget;
+import net.minecraft.entity.ai.EntityAILookIdle;
+import net.minecraft.entity.ai.EntityAIMoveThroughVillage;
+import net.minecraft.entity.ai.EntityAIMoveTowardsRestriction;
+import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
+import net.minecraft.entity.ai.EntityAISwimming;
+import net.minecraft.entity.ai.EntityAIWander;
+import net.minecraft.entity.ai.EntityAIWatchClosest;
+import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.entity.passive.EntityPig;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.world.World;
 
-public class EntityInfPig extends EntityInf {
-    
+public class EntityInfPig extends EntityMob {
     public EntityInfPig(World par1World) {
         super(par1World);
         this.setSize(0.9F, 0.9F);
+        this.tasks.addTask(0, new EntityAISwimming(this));
+        this.tasks.addTask(1, new EntityAIAttackOnCollide(this, EntityPlayer.class, 1.0D, false));
+        this.tasks.addTask(2, new EntityAIMoveTowardsRestriction(this, 1.0D));
+        this.tasks.addTask(3, new EntityAIMoveThroughVillage(this, 1.0D, false));
+        this.tasks.addTask(4, new EntityAIWander(this, 1.0D));
+        this.tasks.addTask(5, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
+        this.tasks.addTask(5, new EntityAILookIdle(this));
+        this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, true));
+        this.targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityPlayer.class, 0, true));
+        this.getEntityData().setBoolean("PHinf", true);
     }
     
     public EntityInfPig(EntityPig ep) {
-        super(ep.worldObj);
-        this.setSize(0.9F, 0.9F);
+        this(ep.worldObj);
         this.setPosition(ep.posX, ep.posY, ep.posZ);
         this.setRotation(ep.rotationYaw, ep.rotationPitch);
+    }
+    protected void func_110147_ax() {
+        super.func_110147_ax();
+        this.func_110148_a(SharedMonsterAttributes.field_111265_b).func_111128_a(40.0D);
+        this.func_110148_a(SharedMonsterAttributes.field_111263_d).func_111128_a(0.23000000417232513D);
+        this.func_110148_a(SharedMonsterAttributes.field_111264_e).func_111128_a(3.0D);
+    }
+    /**
+     * Returns true if the newer Entity AI code should be run
+     */
+    protected boolean isAIEnabled()
+    {
+        return true;
     }
     
     @Override
