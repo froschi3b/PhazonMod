bb2820cf631f1e0f399786edc5f416b2d11284e2
froschi3b <froschi3b@gmail.com>
2013-08-03 02:17:49 +0200

    Update

--- ph_common/at/flabs/mods/phazon/EventHandle.java
+++ ph_common/at/flabs/mods/phazon/EventHandle.java
@@ -1,4 +1,13 @@
 package at.flabs.mods.phazon;
 
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import net.minecraftforge.event.ForgeSubscribe;
+
 public class EventHandle {
+    @ForgeSubscribe
+    public void onRenderHealthBar(RenderGameOverlayEvent.Post evt) {
+        if (evt.type == RenderGameOverlayEvent.ElementType.HEALTH) {
+            
+        }
+    }
 }
