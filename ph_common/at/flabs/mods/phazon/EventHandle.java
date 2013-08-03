229a010ff2809708be5916946994d5f31b12873f
froschi3b <froschi3b@gmail.com>
2013-08-03 10:15:13 +0200

    Update

--- ph_common/at/flabs/mods/phazon/EventHandle.java
+++ ph_common/at/flabs/mods/phazon/EventHandle.java
@@ -1,13 +1,17 @@
 package at.flabs.mods.phazon;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.event.ForgeSubscribe;
 
 public class EventHandle {
+    private static final ResourceLocation guiTex = new ResourceLocation("phazonmod","textures/gui/hud.png");
     @ForgeSubscribe
     public void onRenderHealthBar(RenderGameOverlayEvent.Post evt) {
         if (evt.type == RenderGameOverlayEvent.ElementType.HEALTH) {
-            //Render Phazon Bar Tomorrow :D
+            Minecraft.getMinecraft().renderEngine.func_110577_a(guiTex);
+            
         }
     }
 }
