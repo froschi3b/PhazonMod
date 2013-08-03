4d688e67d5f941c92d86aa02c7cdedf6cf7dfb6c
froschi3b <froschi3b@gmail.com>
2013-08-03 13:57:06 +0200

    Update

--- ph_common/at/flabs/mods/phazon/network/NetHandle.java
+++ ph_common/at/flabs/mods/phazon/network/NetHandle.java
@@ -1,13 +1,53 @@
 package at.flabs.mods.phazon.network;
 
+import net.minecraft.network.INetworkManager;
+import net.minecraft.network.NetLoginHandler;
 import net.minecraft.network.packet.NetHandler;
 import net.minecraft.network.packet.Packet131MapData;
+import net.minecraft.network.packet.Packet1Login;
+import net.minecraft.server.MinecraftServer;
+import cpw.mods.fml.common.network.IConnectionHandler;
 import cpw.mods.fml.common.network.ITinyPacketHandler;
+import cpw.mods.fml.common.network.Player;
 
-public class NetHandle implements ITinyPacketHandler{
+public class NetHandle implements ITinyPacketHandler,IConnectionHandler{
 
     @Override
     public void handle(NetHandler handler, Packet131MapData mapData) {
+        
+    }
+
+    @Override
+    public void playerLoggedIn(Player player, NetHandler netHandler, INetworkManager manager) {
+        
+    }
+
+    @Override
+    public String connectionReceived(NetLoginHandler netHandler, INetworkManager manager) {
+        return null;
+    }
+
+    @Override
+    public void connectionOpened(NetHandler netClientHandler, String server, int port, INetworkManager manager) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void connectionOpened(NetHandler netClientHandler, MinecraftServer server, INetworkManager manager) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void connectionClosed(INetworkManager manager) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void clientLoggedIn(NetHandler clientHandler, INetworkManager manager, Packet1Login login) {
+        // TODO Auto-generated method stub
         
     }
     
