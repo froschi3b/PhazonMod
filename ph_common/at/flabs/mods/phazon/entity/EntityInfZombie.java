67be27ddc0b1fdcac69a8d00901b5d5a1b5448de
froschi3b <froschi3b@gmail.com>
2013-08-03 19:59:25 +0200

    Update

--- ph_common/at/flabs/mods/phazon/entity/EntityInfZombie.java
+++ ph_common/at/flabs/mods/phazon/entity/EntityInfZombie.java
@@ -1,76 +1,26 @@
 package at.flabs.mods.phazon.entity;
 
-import net.minecraft.entity.ai.EntityAIAttackOnCollide;
-import net.minecraft.entity.ai.EntityAIHurtByTarget;
-import net.minecraft.entity.ai.EntityAILookIdle;
-import net.minecraft.entity.ai.EntityAIMoveThroughVillage;
-import net.minecraft.entity.ai.EntityAIMoveTowardsRestriction;
-import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
-import net.minecraft.entity.ai.EntityAISwimming;
-import net.minecraft.entity.ai.EntityAIWander;
-import net.minecraft.entity.ai.EntityAIWatchClosest;
+import net.minecraft.entity.EnumCreatureAttribute;
 import net.minecraft.entity.monster.EntityZombie;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
-public class EntityInfZombie extends EntityInf{
+public class EntityInfZombie extends EntityZombie{
 
-    public EntityInfZombie(World par1World) {
-        super(par1World);
-        this.tasks.addTask(0, new EntityAISwimming(this));
-        this.tasks.addTask(1, new EntityAIAttackOnCollide(this, EntityPlayer.class, 1.0D, false));
-        this.tasks.addTask(2, new EntityAIMoveTowardsRestriction(this, 1.0D));
-        this.tasks.addTask(3, new EntityAIMoveThroughVillage(this, 1.0D, false));
-        this.tasks.addTask(4, new EntityAIWander(this, 1.0D));
-        this.tasks.addTask(5, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
-        this.tasks.addTask(5, new EntityAILookIdle(this));
-        this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, true));
-        this.targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityPlayer.class, 0, true));
+    public EntityInfZombie(World world) {
+        super(world);
     }
     public EntityInfZombie(EntityZombie ep){
         this(ep.worldObj);
         this.setPosition(ep.posX, ep.posY, ep.posZ);
-        this.setRotation(ep.rotationYaw, ep.rotationPitch);
+        ItemStack[] inv=ep.getLastActiveItems();
+        for (int i=0;i<inv.length;i++){
+            this.setCurrentItemOrArmor(i, inv[i]);
+        }
     }
-    /**
-     * Returns the sound this mob makes while it's alive.
-     */
-    protected String getLivingSound()
+    public EnumCreatureAttribute getCreatureAttribute()
     {
-        return "mob.zombie.say";
-    }
-
-    /**
-     * Returns the sound this mob makes when it is hurt.
-     */
-    protected String getHurtSound()
-    {
-        return "mob.zombie.hurt";
-    }
-
-    /**
-     * Returns the sound this mob makes on death.
-     */
-    protected String getDeathSound()
-    {
-        return "mob.zombie.death";
-    }
-
-    /**
-     * Plays step sound at given x, y, z for the entity
-     */
-    protected void playStepSound(int par1, int par2, int par3, int par4)
-    {
-        this.playSound("mob.zombie.step", 0.15F, 1.0F);
-    }
-
-    /**
-     * Returns the item ID for the item the mob drops on death.
-     */
-    protected int getDropItemId()
-    {
-        return Item.rottenFlesh.itemID;
+        return EnumCreatureAttribute.UNDEFINED;
     }
     
 }
