c28f608fe9373a8aeb64e5644a884bb845277726
froschi3b <froschi3b@gmail.com>
2013-08-03 10:29:57 +0200

    Update

--- ph_common/at/flabs/mods/phazon/EventHandle.java
+++ ph_common/at/flabs/mods/phazon/EventHandle.java
@@ -1,6 +1,7 @@
 package at.flabs.mods.phazon;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.event.ForgeSubscribe;
@@ -11,6 +12,9 @@ public class EventHandle {
     public void onRenderHealthBar(RenderGameOverlayEvent.Post evt) {
         if (evt.type == RenderGameOverlayEvent.ElementType.HEALTH) {
             Minecraft.getMinecraft().renderEngine.func_110577_a(guiTex);
+            int wid=Minecraft.getMinecraft().currentScreen.width/2;
+            int hid=Minecraft.getMinecraft().currentScreen.height/2;
+            Minecraft.getMinecraft().currentScreen.drawTexturedModalRect(x, y, u, v, wid, hei);
             
         }
     }
