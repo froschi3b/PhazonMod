0dbcd54839165bdb5db4e9c469064321512c12c4
froschi3b <froschi3b@gmail.com>
2013-08-03 11:58:23 +0200

    Update

--- ph_common/at/flabs/mods/phazon/EventHandle.java
+++ ph_common/at/flabs/mods/phazon/EventHandle.java
@@ -1,8 +1,10 @@
 package at.flabs.mods.phazon;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.ScaledResolution;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.GuiIngameForge;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.event.ForgeSubscribe;
 
@@ -10,15 +12,21 @@ public class EventHandle {
     private static final ResourceLocation guiTex = new ResourceLocation(Vars.texdir,Vars.hud);
     private ScaledResolution res;
     @ForgeSubscribe
-    public void onRenderHealthBar(RenderGameOverlayEvent.Pre evt) {
+    public void onRenderHealthBar(RenderGameOverlayEvent.Post evt) {
         if (evt.type == RenderGameOverlayEvent.ElementType.HEALTH) {
             Minecraft mc =Minecraft.getMinecraft();
             res = new ScaledResolution(mc.gameSettings, mc.displayWidth, mc.displayHeight);
             int width=res.getScaledWidth();
-            int height=res.getScaledHeight();
-
+            //int height=res.getScaledHeight();
+            
+            int y = 0;
+            if(GuiIngameForge.renderBossHealth){
+                y=20;
+            }
+            
             mc.renderEngine.func_110577_a(guiTex);
-            mc.ingameGUI.drawTexturedModalRect(0, 0, 0, 0, 128, 16);
+            mc.ingameGUI.drawTexturedModalRect(width/2-64, y, 0, 0, 128, 16);
+            mc.func_110434_K().func_110577_a(Gui.field_110324_m);
         }
     }
 }
