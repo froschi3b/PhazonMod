19df993a96810ae9b9ece6f9c66d623495c6b059
froschi3b <froschi3b@gmail.com>
2013-08-04 18:54:37 +0200

    Update

---
+++ ph_common/at/flabs/mods/phazon/client/ModelInfSkeleton.java
@@ -0,0 +1,15 @@
+package at.flabs.mods.phazon.client;
+
+import at.flabs.mods.phazon.entity.EntityInfSkeleton;
+import net.minecraft.client.model.ModelSkeleton;
+import net.minecraft.entity.EntityLivingBase;
+
+public class ModelInfSkeleton extends ModelSkeleton{
+    public ModelInfSkeleton(){
+        super();
+    }
+    public void setLivingAnimations(EntityLivingBase par1EntityLivingBase, float par2, float par3, float par4)
+    {
+        this.aimedBow = ((EntityInfSkeleton)par1EntityLivingBase).getSkeletonType() == 1;
+    }
+}
--- ph_common/at/flabs/mods/phazon/client/RenderInfSkeleton.java
+++ ph_common/at/flabs/mods/phazon/client/RenderInfSkeleton.java
@@ -4,7 +4,6 @@ import at.flabs.mods.phazon.Vars;
 import at.flabs.mods.phazon.entity.EntityInfSkeleton;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
-import net.minecraft.client.model.ModelSkeleton;
 import net.minecraft.client.renderer.entity.RenderBiped;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLiving;
@@ -14,49 +13,36 @@ import net.minecraft.util.ResourceLocation;
 import org.lwjgl.opengl.GL11;
 
 @SideOnly(Side.CLIENT)
-public class RenderInfSkeleton extends RenderBiped
-{
+public class RenderInfSkeleton extends RenderBiped {
     private static final ResourceLocation tex = new ResourceLocation(Vars.texdir, "textures/entity/infskeleton.png");
-
-    public RenderInfSkeleton()
-    {
-        super(new ModelSkeleton(), 0.5F);
+    
+    public RenderInfSkeleton() {
+        super(new ModelInfSkeleton(), 0.5F);
     }
-
-    protected void preRenderCallback(EntityInfSkeleton par1EntitySkeleton, float par2)
-    {
-        if (par1EntitySkeleton.getSkeletonType() == 1)
-        {
+    
+    protected void preRenderCallback(EntityInfSkeleton par1EntitySkeleton, float par2) {
+        if (par1EntitySkeleton.getSkeletonType() == 1) {
             GL11.glScalef(1.2F, 1.2F, 1.2F);
         }
     }
-
-    protected void func_82422_c()
-    {
+    
+    protected void func_82422_c() {
         GL11.glTranslatef(0.09375F, 0.1875F, 0.0F);
     }
-
-    protected ResourceLocation func_110860_a(EntityInfSkeleton par1EntitySkeleton)
-    {
+    
+    protected ResourceLocation func_110856_a(EntityLiving par1EntityLiving) {
         return tex;
     }
-
-    protected ResourceLocation func_110856_a(EntityLiving par1EntityLiving)
-    {
-        return this.func_110860_a((EntityInfSkeleton)par1EntityLiving);
-    }
-
+    
     /**
-     * Allows the render to do any OpenGL state modifications necessary before the model is rendered. Args:
-     * entityLiving, partialTickTime
+     * Allows the render to do any OpenGL state modifications necessary before
+     * the model is rendered. Args: entityLiving, partialTickTime
      */
-    protected void preRenderCallback(EntityLivingBase par1EntityLivingBase, float par2)
-    {
-        this.preRenderCallback((EntityInfSkeleton)par1EntityLivingBase, par2);
+    protected void preRenderCallback(EntityLivingBase par1EntityLivingBase, float par2) {
+        this.preRenderCallback((EntityInfSkeleton) par1EntityLivingBase, par2);
     }
-
-    protected ResourceLocation func_110775_a(Entity par1Entity)
-    {
-        return this.func_110860_a((EntityInfSkeleton)par1Entity);
+    
+    protected ResourceLocation func_110775_a(Entity par1Entity) {
+        return tex;
     }
 }
