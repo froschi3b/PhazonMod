2923b1c8dd511a957ac09bc9827ff946b33ea594
froschi3b <froschi3b@gmail.com>
2013-08-04 02:32:55 +0200

    Update

--- ph_common/at/flabs/mods/phazon/client/RenderPlasma.java
+++ ph_common/at/flabs/mods/phazon/client/RenderPlasma.java
@@ -1,6 +1,7 @@
 package at.flabs.mods.phazon.client;
 
 import at.flabs.mods.phazon.Vars;
+import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.ResourceLocation;
@@ -8,9 +9,11 @@ import net.minecraft.util.ResourceLocation;
 public class RenderPlasma extends Render {
     private static ResourceLocation tex = new ResourceLocation(Vars.texdir, "textures/entity/plasma.png");
     
+    private static ModelBase model=new ModelPlasma();
+    
     @Override
     public void doRender(Entity entity, double d0, double d1, double d2, float f, float f1) {
-        
+        model.render(entity, d0, d1, d2, f, f1, 0);
     }
     
     @Override
