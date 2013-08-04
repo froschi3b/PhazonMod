bbbf2a667898a0a6bc3c5586de5df64bfcbeae0f
froschi3b <froschi3b@gmail.com>
2013-08-04 20:48:42 +0200

    Update

--- ph_common/at/flabs/mods/phazon/item/ItemPhazonCanon.java
+++ ph_common/at/flabs/mods/phazon/item/ItemPhazonCanon.java
@@ -2,22 +2,18 @@ package at.flabs.mods.phazon.item;
 
 import at.flabs.mods.phazon.Util;
 import at.flabs.mods.phazon.Vars;
+import at.flabs.mods.phazon.entity.EntityPlasma;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
-import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.projectile.EntityArrow;
 import net.minecraft.item.EnumAction;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Icon;
 import net.minecraft.world.World;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.player.ArrowLooseEvent;
-import net.minecraftforge.event.entity.player.ArrowNockEvent;
 
 public class ItemPhazonCanon extends Item {
     @SideOnly(Side.CLIENT)
@@ -31,73 +27,53 @@ public class ItemPhazonCanon extends Item {
         this.func_111206_d(Vars.texdir + ":" + Vars.unlocalizedPhazonCanon);
     }
     
-    /**
-     * called when the player releases the use item button. Args: itemstack,
-     * world, entityplayer, itemInUseCount
-     */
-    public void onPlayerStoppedUsing(ItemStack par1ItemStack, World par2World, EntityPlayer par3EntityPlayer, int par4) {
-        int j = this.getMaxItemUseDuration(par1ItemStack) - par4;
+    public ItemStack onEaten(ItemStack is, World world, EntityPlayer ep) {
+        int j = this.getMaxItemUseDuration(is);
         
-        ArrowLooseEvent event = new ArrowLooseEvent(par3EntityPlayer, par1ItemStack, j);
-        MinecraftForge.EVENT_BUS.post(event);
-        if (event.isCanceled()) {
-            return;
-        }
-        j = event.charge;
+        boolean flag = EnchantmentHelper.getEnchantmentLevel(Enchantment.infinity.effectId, is) > 0;
         
-        boolean flag = par3EntityPlayer.capabilities.isCreativeMode || EnchantmentHelper.getEnchantmentLevel(Enchantment.infinity.effectId, par1ItemStack) > 0;
-        
-        if (flag || par3EntityPlayer.inventory.hasItem(Item.arrow.itemID)) {
-            float f = (float) j / 20.0F;
+        if (ep.capabilities.isCreativeMode || ep.inventory.hasItem(Item.arrow.itemID)) {
+            float f = (float) j;
             f = (f * f + f * 2.0F) / 3.0F;
-            
-            if ((double) f < 0.1D) {
-                return;
-            }
             
             if (f > 1.0F) {
                 f = 1.0F;
             }
             
-            EntityArrow entityarrow = new EntityArrow(par2World, par3EntityPlayer, f * 2.0F);
+            EntityPlasma entityplasma = new EntityPlasma(world, ep, f * 2.0F);
             
             if (f == 1.0F) {
-                entityarrow.setIsCritical(true);
+                entityplasma.setIsCritical(true);
             }
             
-            int k = EnchantmentHelper.getEnchantmentLevel(Enchantment.power.effectId, par1ItemStack);
+            int k = EnchantmentHelper.getEnchantmentLevel(Enchantment.power.effectId, is);
             
             if (k > 0) {
-                entityarrow.setDamage(entityarrow.getDamage() + (double) k * 0.5D + 0.5D);
+                entityplasma.setDamage(entityplasma.getDamage() + (double) k * 0.5D + 0.5D);
             }
             
-            int l = EnchantmentHelper.getEnchantmentLevel(Enchantment.punch.effectId, par1ItemStack);
+            int l = EnchantmentHelper.getEnchantmentLevel(Enchantment.punch.effectId, is);
             
             if (l > 0) {
-                entityarrow.setKnockbackStrength(l);
+                entityplasma.setKnockbackStrength(l);
             }
             
-            if (EnchantmentHelper.getEnchantmentLevel(Enchantment.flame.effectId, par1ItemStack) > 0) {
-                entityarrow.setFire(100);
+            if (EnchantmentHelper.getEnchantmentLevel(Enchantment.flame.effectId, is) > 0) {
+                entityplasma.setFire(100);
             }
             
-            par1ItemStack.damageItem(1, par3EntityPlayer);
-            par2World.playSoundAtEntity(par3EntityPlayer, "random.bow", 1.0F, 1.0F / (itemRand.nextFloat() * 0.4F + 1.2F) + f * 0.5F);
+            is.damageItem(1, ep);
+            world.playSoundAtEntity(ep, "random.bow", 1.0F, 1.0F / (itemRand.nextFloat() * 0.4F + 1.2F) + f * 0.5F);
             
-            if (flag) {
-                entityarrow.canBePickedUp = 2;
-            } else {
-                par3EntityPlayer.inventory.consumeInventoryItem(Item.arrow.itemID);
+            if (!ep.capabilities.isCreativeMode) {
+                ep.inventory.consumeInventoryItem(Item.arrow.itemID);
             }
             
-            if (!par2World.isRemote) {
-                par2World.spawnEntityInWorld(entityarrow);
+            if (!world.isRemote) {
+                world.spawnEntityInWorld(entityplasma);
             }
         }
-    }
-    
-    public ItemStack onEaten(ItemStack par1ItemStack, World par2World, EntityPlayer par3EntityPlayer) {
-        return par1ItemStack;
+        return is;
     }
     
     /**
