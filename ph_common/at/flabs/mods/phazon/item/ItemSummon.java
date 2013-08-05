ab1d190a11f09af998f8335ab0a377cd0908c0b3
froschi3b <froschi3b@gmail.com>
2013-08-05 16:00:38 +0200

    Update

--- ph_common/at/flabs/mods/phazon/item/ItemSummon.java
+++ ph_common/at/flabs/mods/phazon/item/ItemSummon.java
@@ -9,34 +9,39 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
 public class ItemSummon extends Item {
-
+    
     public ItemSummon(int id) {
         super(id);
         this.setCreativeTab(CreativeTabs.tabMisc);
         this.func_111206_d(Vars.texdir + ":" + Vars.unlocalizedSummon);
     }
-
+    
     public ItemStack onEaten(ItemStack is, World world, EntityPlayer ep) {
-        double x=ep.posX;//-100D + world.rand.nextInt(200);
-        double z=ep.posX;//-100D + world.rand.nextInt(200);
-        double y=ep.posY;//world.getHeightValue((int)x, (int)z);
-        
-        PhazonExplosion explosion = new PhazonExplosion(world, ep, x, y, z, 9);
-        explosion.isSmoking = false;
-        explosion.doExplosionA();
-        explosion.doExplosionB(true);
+        if (!world.isRemote) {
+            double x = ep.posX -100D + world.rand.nextInt(200);
+            double z = ep.posX -100D + world.rand.nextInt(200);
+            double y = world.getHeightValue((int)x, (int)z);
+            
+            PhazonExplosion explosion = new PhazonExplosion(world, ep, x,y, z, 9);
+            explosion.isSmoking = true;
+            explosion.doExplosionA();
+            explosion.doExplosionB(true);
+        }
         is.stackSize--;
         return is;
     }
+    
     /**
      * How long it takes to use or consume an item
      */
     public int getMaxItemUseDuration(ItemStack par1ItemStack) {
         return 20;
     }
+    
     public EnumAction getItemUseAction(ItemStack par1ItemStack) {
         return EnumAction.bow;
     }
+    
     public ItemStack onItemRightClick(ItemStack is, World world, EntityPlayer player) {
         player.setItemInUse(is, this.getMaxItemUseDuration(is));
         return is;
